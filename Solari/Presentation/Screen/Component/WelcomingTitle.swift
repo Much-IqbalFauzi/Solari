40c92e92762dee679cab4bfdbe6fcca4c51e5bd8
Ailsa Anarghia <ailsanarghia@10-60-39-3.0100.bsd.id.iosda.org>
2025-03-26 14:20:41 +0700

    update WelcomingTitle

---
+++ Solari/Presentation/Screen/Component/WelcomingTitle.swift
@@ -0,0 +1,44 @@
+//
+//  WelcomingTitle.swift
+//  Solari
+//
+//  Created by Ailsa Anarghia on 26/03/25.
+//
+
+import SwiftUI
+
+struct WelcomingTitle: View {
+    
+    func getGreetingMessage() -> String {
+            let currentHour = Calendar.current.component(.hour, from: Date())
+            
+            switch currentHour {
+            case 0..<12:
+                return "Good MorningSobari!"
+            case 12..<17:
+                return "Good Afternoon Sobari!"
+            case 17..<21:
+                return "Good Evening Sobari!"
+            default:
+                return "Good Night Sobari!"
+            }
+        }
+    
+    var body: some View {
+        VStack {
+            Text(getGreetingMessage())
+                .font(.largeTitle)
+                .fontWeight(.semibold)
+                .padding()
+            Spacer()
+        }
+        .padding(.top, 50 )
+        .offset(x:-5,y:0)
+        
+        
+    }
+}
+
+#Preview {
+    WelcomingTitle()
+}
