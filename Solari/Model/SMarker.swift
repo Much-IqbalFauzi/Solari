4945ebf31aa3895c7d41f8fb1eceed7091a69c50
Muchamad Iqbal Fauzi <fauzi@Muchamads-MacBook-Pro.local>
2025-04-03 23:07:24 +0700

    route list sample data

---
+++ Solari/Model/SMarker.swift
@@ -0,0 +1,19 @@
+//
+//  SMarker.swift
+//  Solari
+//
+//  Created by Muchamad Iqbal Fauzi on 03/04/25.
+//
+
+import MapKit
+import Foundation
+
+struct sMarker {
+    var name: String
+    var coordinate: CLLocationCoordinate2D
+    
+    init(name: String, coordinate: CLLocationCoordinate2D) {
+        self.name = name
+        self.coordinate = coordinate
+    }
+}
---
+++ Solari/ViewModel/SelectRouteViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  SelectRouteViewModel.swift
+//  Solari
+//
+//  Created by Muchamad Iqbal Fauzi on 03/04/25.
+//
+
+import SwiftUI
+
+class SelectRouteViewModel: ObservableObject {
+    
+    
+}
