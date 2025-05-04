e4ce37c1822cc21f3b376cd5840780e237c6f543
Muchamad Iqbal Fauzi <fauzi@Muchamads-MacBook-Pro.local>
2025-05-04 18:09:15 +0700

    Initial Commit

---
+++ SolariFN/SolariFNApp.swift
@@ -0,0 +1,17 @@
+//
+//  SolariFNApp.swift
+//  SolariFN
+//
+//  Created by Muchamad Iqbal Fauzi on 04/05/25.
+//
+
+import SwiftUI
+
+@main
+struct SolariFNApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
